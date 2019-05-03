35cbd99b42b83fb4edbf7ddd77bfd963c8578ab3
Chenger1 <exs2199@gmail.com>
2019-05-03 22:42:30 +0300

    Create requirements.txt

--- TimeTracking/settings.py
+++ TimeTracking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'Secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'timetracking',
         'USER':'nitron',
-        'PASSWORD':'password',
+        'PASSWORD':'pass',
         'HOST': 'localhost',
         'PORT':'',
     }
