6b801cbb73bc46a3ac9a9bc21648127babc800f8
Chenger1 <exs2199@gmail.com>
2019-05-03 23:39:38 +0300

    Adds deploy files

--- TimeTracking/settings.py
+++ TimeTracking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't00c_8a+u764#vvn5e4m%vplff#()vad*_!jg(c4x54d&#4bb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
