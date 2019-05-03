863cd765220a3d8e76f249fef6194087a188dc5b
Chenger1 <exs2199@gmail.com>
2019-05-03 23:34:54 +0300

    Database changed

--- TimeTracking/settings.py
+++ TimeTracking/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'TimeTracking.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'timetracking',
-        'USER':'nitron',
-        'PASSWORD':'superuser',
-        'HOST': 'localhost',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
